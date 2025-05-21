cd7dbb104abe64549536ff2c55b022835714f33b
adithya28-g <adithyaganesh916@gmail.com>
2025-05-21 22:11:47 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-console.log(" this is js for yencryto ");
 document.addEventListener('DOMContentLoaded', function() {
     // Set current year in footer
     //document.getElementById('current-year').textContent = new Date().getFullYear();
